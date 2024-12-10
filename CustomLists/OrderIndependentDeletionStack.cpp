f22fd3a2814ae8d542a5f1180284b9cb01d5661a
JJBcku <154745800+JJBcku@users.noreply.github.com>
2024-12-10 12:53:38 +0100

    _nextVectorID bugfix

--- CustomLists/OrderIndependentDeletionStack.cpp
+++ CustomLists/OrderIndependentDeletionStack.cpp
@@ -1,4 +1,1 @@
 #include "OrderIndependentDeletionStack.h"
-
-template<class T>
-IDType OrderIndependentDeletionStack<T>::_nextVectorID = std::numeric_limits<IDType>::lowest();
--- CustomLists/UnsortedIDVector.cpp
+++ CustomLists/UnsortedIDVector.cpp
@@ -1,4 +1,1 @@
 #include "UnsortedIDVector.h"
-
-template<class T>
-IDType UnsortedIDVector<T>::_nextVectorID = std::numeric_limits<IDType>::lowest();
